d4522218933d64506631c0462230ebbec1870250
Kellen Schmidt <kellenschmidt31@gmail.com>
2017-09-21 15:17:22 -0500

    Please ignore this commit

---
+++ AudioLab/DopplerViewController.h
@@ -0,0 +1,13 @@
+//
+//  DopplerViewController.h
+//  AudioLab
+//
+//  Created by Kellen Schmidt on 9/19/17.
+//  Copyright © 2017 Eric Larson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DopplerViewController : UIViewController
+
+@end
---
+++ AudioLab/DopplerViewController.m
@@ -0,0 +1,73 @@
+//
+//  DopplerViewController.m
+//  AudioLab
+//
+//  Created by Kellen Schmidt on 9/19/17.
+//  Copyright © 2017 Eric Larson. All rights reserved.
+//
+
+#import "DopplerViewController.h"
+#import "Novocaine.h"
+#import "CircularBuffer.h"
+#import "FFTHelper.h"
+
+#define BUFFER_SIZE 2048
+
+@interface DopplerViewController ()
+@property (strong, nonatomic) Novocaine *audioManager;
+@property (strong, nonatomic) CircularBuffer *buffer;
+@property (strong, nonatomic) FFTHelper *fftHelper;
+@end
+
+@implementation DopplerViewController
+
+#pragma mark Lazy Instantiation
+-(Novocaine*)audioManager
+{
+    if(!_audioManager)
+    {
+        _audioManager = [Novocaine audioManager];
+    }
+    return _audioManager;
+}
+
+-(CircularBuffer*)buffer
+{
+    if(!_buffer)
+    {
+        _buffer = [[CircularBuffer alloc]initWithNumChannels:1 andBufferSize:BUFFER_SIZE];
+    }
+    return _buffer;
+}
+
+-(FFTHelper*)fftHelper
+{
+    if(!_fftHelper)
+    {
+        _fftHelper = [[FFTHelper alloc]initWithFFTSize:BUFFER_SIZE];
+    }
+    
+    return _fftHelper;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
